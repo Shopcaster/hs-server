357cbc12868aa3da2438ccc8b0c8f5c266231f51
Patrick Stein <lylepstein@gmail.com>
2011-07-08 13:00:26 -0400

    Working on the data layer in the api interface

--- interface/api.js
+++ interface/api.js
@@ -1,4 +1,4 @@
-// SPANDEX - A temporary name for our flexible protocol library.
+// Seig heil!
 this.spandex = null;
 
 // TODO
@@ -7,6 +7,8 @@ this.spandex = null;
 // * Presence
 // * Data read layer
 // * Data write layer
+// * Reconnect handling
+// * Remove deauth
 
 //
 // Expects the following to exist:
@@ -45,6 +47,8 @@ EventEmitter.prototype.on = function(event, listener) {
   if (!this._listeners[event]) this._listeners[event] = [];
   this._listeners[event].push(listener);
 
+  this.emit('newListener', event, listener);
+
   return this;
 };
 EventEmitter.prototype.once = function(event, listener) {
@@ -52,14 +56,19 @@ EventEmitter.prototype.once = function(event, listener) {
   if (!this._onceListeners[event]) this._onceListeners[event] = [];
   this._onceListeners[event].push(listeners);
 
+  this.emit('newListener', event, listener);
+
   return this;
 };
 EventEmitter.prototype.removeListener = function(event, listener) {
-  if (this._listeners && this._listeners[event]);
-    //todo
+  if (this._listeners && this._listeners[event]) {
+    var n = this._listeners[event].indexOf(listener);
+    if (n >= 0) this._listeners[event].splice(n, 1);
 
-  if (this._onceListeners && this._onceListeners[event]);
-    //todo
+  } else if (this._onceListeners && this._onceListeners[event]) {
+    var n = this._onceListeners[event].indexOf(listener);
+    if (n >= 0) this._onceListeners[event].splice(n, 1);
+  }
 
   return this;
 };
@@ -90,6 +99,10 @@ EventEmitter.prototype.emit = function() {
 
   return this;
 };
+// We have no concept of max listeners...
+EventEmitter.prototype.setMaxListeners = function() {};
+// NYI
+EventEmitter.prototype.listeners = function(event) { throw new Error('Not Yet Implemented') };
 
 //
 // Initialize the spandex object
@@ -133,7 +146,7 @@ var log = function() {
 //
 // Messaging util
 //
-var messaging = {};
+var messaging = new EventEmitter();
 (function() {
   // Message ID
   messaging.id = 1;
@@ -160,9 +173,15 @@ var messaging = {};
     // Log the message if we're configured to do so
     if (spandex.logging[msg.type]) log(msg.type, msg.data);
 
-    // Fire the appropriate callback
-    messaging.callbacks[msg.data.id](msg.data);
-    delete messaging.callbacks[msg.data.id];
+    // If the message is a response, fire the appropriate callback
+    if (msg.type == 'response' && messaging.callbacks[msg.data.id]) {
+      messaging.callbacks[msg.data.id](msg.data);
+      delete messaging.callbacks[msg.data.id];
+
+    // Otherwise, pass it on to the correct handler by firing an event
+    } else {
+      messaging.emit(msg,type, msg.data);
+    }
   };
   // Sends a message
   messaging.send = function(msg, data, callback) {
@@ -349,23 +368,225 @@ spandex.auth.user = null;
     'user', 'users'
   ];
 
+  // Active subscriptions
+
   // Initialize he data layer
   spandex.data = {};
   spandex.models = {};
 
+  // Active subscriptions
+  var subs = {};
+
+  // Base subscription class
+  var Sub = function(key) {
+    // TODO - this is broken
+    var self = this;
+
+    this.ready = false;
+    this.key = key;
+    // Add to the active subscriptions list
+    subs[key] = this;
+    // Set the reference count
+    this.refs = 0;
+    // Make the subscription, and notify the _sub listeners
+    this._sub(function(data) {
+      this.ready = true;
+
+      self.emit('ready', data);
+      self.removeAllListeners('ready');
+    });
+  };
+  Sub.prototype = new EventEmitter();
+  Sub.prototype._sub = function(callback) {
+    var self = this;
+
+    messaging.send('sub', {key: key}, function(err, data) {
+      // Handle errors by deleting the subscription
+      if (err) {
+        delete subs[key];
+        console.log('Error while subscribing:', err);
+        return;
+      }
+      // Log bad subs to console
+      if (data === false) {
+        delete subs[key];
+        console.log('Attempted to subscribe to nonexistent key:', key);
+        return;
+      }
+      // Store the data
+      self.data = data;
+
+      // Call the callback
+      if (callback) callback(data);
+    });
+  };
+  Sub.prototype.resub = function() {
+    var self = this;
+
+    this._sub(function(data) {
+      self.update(data);
+    });
+  };
+  Sub.prototype.update = function(data) { throw new Error('NYI') };
+  Sub.prototype.destroy = function() {
+    // Unsub this sub
+    messaging.send('unsub', {key: key}, function(err, data) {
+      // Handle errors by failing
+      if (err) return console.log('Error while unsubbing:', err);
+    });
+
+    // And remove ourselves from the list
+    delete subs[key];
+  };
+  Sub.prototype.retain = function() { this.refs++ };
+  Sub.prototype.release = function() {
+    if (--this.refs < 1) {
+      // Don't remove the subscription right away.  Instead, hold it
+      // for about 10s in case anybody else wants it in that time.
+      var self = this;
+      setTimeout(function() {
+        if (self.refs < 1) self.destroy();
+      }, 10 * 1000);
+    }
+  };
+
+  // Subscription on a model
+  var ModelSub = function() {
+    Sub.apply(this, Array.prototype.slice.call(arguments));
+  };
+  ModelSub.prototype = new Sub();
+  ModelSub.prototype.update = function(data) {
+    for (var i in data) if (data.hasOwnProperty(i)) {
+      // Only update fields if they're different
+      if (this.data[i] != data[i]) {
+        // Update the field in our internal data storage
+        this.data[i] = data[i];
+        // Fire the relevant callback
+        this.emit('field', i, data[i]);
+      }
+    }
+  };
+
+  // Subscription on a relation
+  var RelationSub = function() {
+    Sub.apply(this, Array.prototype.slice.call(arguments));
+    this.data = [];
+  };
+  RelationSub.prototype = new Sub();
+  RelationSub.prototype.update = function(data) {
+    // Add elements
+    for (var i=0; i<data.add.length; i++) {
+      var x = data.add[i];
+      if (this.data.indexOf(x) == -1) this.data.push(x);
+    }
+
+    // Remove elements
+    for (var i=0; i<data.remove.length; i++) {
+      var x = data.remove[i];
+      var n = this.data.indexOf(x);
+      if (n >= 0)
+        this.data.splice(n, 1);
+    };
+
+    // Fire the appropriate events
+    if (data.add.length)
+      this.emit('add', data.add);
+    if (data.remove.length)
+      this.emit('remove', data.remove);
+  };
+
+  // Register a handler for `pub` messages so that we can update
+  // the relevant sub
+  messaging.on('pub', function(data) {
+    if (!subs[data.key]) return console.log('Error: Dangling sub');
+
+    subs[data.key].update(data);
+  });
+
   // The model class
-  spandex.models.Model = function(type) { this._type = type; };
+  spandex.models.Model = function(type) {
+    this._type = type;  // Model type (e.g. 'listing')
+    this._slis = [];    // Listeners we've registered on the sub
+    this._sub = null;   // The current subscription.  Only set if hot.
+    this._levents = {}; // The events currently being listened on this model.
+
+    var self = this;
+    this.on('newListener', function(event, listener) {
+      if (event != 'newListeners') self._levents[event] = true;
+    });
+  };
   spandex.models.Model.prototype = new EventEmitter();
   spandex.models.Model.prototype.related = {};
+  spandex.models.Model.prototype.heat = function() {
+    if (this.hot) throw new Error('Model is already hot');
+
+    // This is a helper function that bootstraps all the data.
+    var bootstrap = function(data) {
+      for (var i in data) if (data.hasOwnProperty(i)) {
+        //only update if needed
+        if (self[i] != data[i]) {
+          self[i] = data[i];
+          self.emit(i, data[i]);
+        }
+      }
+    };
+
+    // Subscribe for changes.
+    var self = this;
+    // Grab an existing sub for this key, or create one
+    this._sub = subs[this.key] || new Sub(this.key);
+    // If the sub isn't ready we should listen on the ready event
+    // so we can update data.
+    if (!this._sub.ready)
+      this._sub.on('ready', bootstrap) && this._slis.push(['ready', bootstrap]);
+    // If the sub is ready, we need to just bootstrap the data from
+    // it directly.
+    else
+      bootstrap(this._sub.data);
+
+    // Listen on field events
+    var field = function(field, val) {
+      // The sub already checks whether the field changed before firing
+      // the field event.
+      self[i] = val;
+      self.emit(i, val);
+    };
+    this._sub.on('field', field);
+
+    // Push the field event listeners so we can remove it
+    this._slis.push(['field', field]);
+
+    // Finally, mark this model as hot
+    this.hot = true;
+    return this;
+  };
+  spandex.models.Model.prototype.freeze = function() {
+    if (!this.hot) throw new Error('Model is already cold');
+
+    // Unsubscribe all registered events on the sub
+    for (var i=0; i<this._slis.length; i++) {
+      var ev = this._slis[i];
+      this._sub.removeListener(ev[0], ev[1]);
+    }
+    this._slis = [];
+    // Decrease the reference count of the base sub
+    this._sub.release();
+    this._sub = null;
+    // Unregister all events registered on this model
+    for (var i=0; i<this._levents.length; i++) {
+      var event = this._levents[i];
+      this.removeAllListeners(event)
+    }
+    this._levents = {};
+
+    // Finally, mark this model as cold
+    this.hot = false;
+    return this;
+  };
 
   // The IDList class
   spandex.models.IDList = function(type) { this._type = type; };
   spandex.models.IDList.prototype = new EventEmitter();
-
-  // Gets a model by ID
-  spandex.data._get = function(id) {
-
-  };
 
   // Data initialization
   for (var i=0; i<config.models.length; i+=2) {
@@ -373,7 +594,10 @@ spandex.auth.user = null;
     var ptype = config.models[i+1];
 
     // Create and register the model for this type
-    var M = function() {};
+    var M = function(key) {
+      this.hot = false;
+      this.key = key;
+    };
     M.prototype = new spandex.models.Model(type);
     spandex.models[type[0].toUpperCase() + type.substr(1)] = M;
 
@@ -393,6 +617,8 @@ spandex.auth.user = null;
 
       // Set the default field
       if (!field) field = type;
+
+      // TODO - make the query
     };
 
     // Create the fetcher
@@ -425,6 +651,13 @@ spandex.auth.user = null;
       // Otherwise, if the thing is an object we want
       // to use the default field (`type`), or the field
       // chain if it was specified
+      if (fields.length == 0) { // Default field
+
+      } else if (fields.length == 1) { // Explicit field
+
+      } else { // Field chain
+
+      }
     };
   };
 
