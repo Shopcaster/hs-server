6da51a89f1ccc3b4af1c40e0b777ebf114e5f4c2
Aron Jones <defrex0@gmail.com>
2011-07-20 17:43:19 -0400

    fix up crosspost page

--- iapi/crosspost.js
+++ iapi/crosspost.js
@@ -1,6 +1,7 @@
 var db = require('../db'),
     models = require('../models')
-    templating = require('../templating');
+    templating = require('../templating')
+    http = require('http');
 
 
 var do404 = function(res) {
@@ -23,9 +24,40 @@ var serve = function(req, res) {
     // If there's no such listing, bail out
     if (err) return do404(res);
 
-    // Otherwise, serve up a straight HTML page with the listing
-    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    res.end(templating['crosspost'].render({listing: listing}));
+    var context = {listing: listing};
+
+    var title = listing.description
+      .replace(/^\s*/, '')
+      .replace(/\s*$/, '')
+      .substr(0, 67)
+      .split(' ');
+    title.pop();
+    title = title.join(' ')+'...';
+    context.title = title;
+
+    http.get({
+      host: 'maps.googleapis.com',
+      port: 80,
+      path: '/maps/api/geocode/json?'
+        +'latlng='+listing.latitude+','+listing.longitude
+        +'&sensor=false'
+    }, function(gRes) {
+      var result = '';
+
+      gRes.on('data', function(data){
+        if (data) result += data;
+      });
+
+      gRes.on('end', function(){
+        result = JSON.parse(result);
+
+        context.address = result.results[0].formatted_address;
+
+        // Otherwise, serve up a straight HTML page with the listing
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        res.end(templating['crosspost'].render(context));
+      });
+    });
   });
 };
 
