06f23a3a8150cf6d37d7c02050110da9fcfce90d
Patrick Stein <lylepstein@gmail.com>
2011-07-22 19:03:00 -0400

    Using formidable for mailgun email parsing, which will allow us to handle emails from Craigslist, among other things

--- iapi/email.js
+++ iapi/email.js
@@ -1,9 +1,14 @@
 var mailgun = require('mailgun'),
-    querystring = require('querystring'),
+    formidable = require('formidable'),
     email = require('../email'),
     templating = require('../templating'),
     db = require('../db'),
     models = require('../models');
+
+var doResp = function(res, num, d) {
+  res.writeHead(num, {'Content-Type': 'text/html; charset=utf-8'});
+  res.end(d);
+};
 
 var serve = function(req, res) {
   var match = req.url.match(/listing\/\d+$/);
@@ -11,69 +16,46 @@ var serve = function(req, res) {
 
   // If we couldn't grab what looks like a listing id from the URL, we
   // should bail.
-  if (!id) {
-    res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'})
-    res.end('Not Found');
-    return;
-  }
+  if (!id) return doResp(res, 404, 'Not Found');
 
-  // Read the data
-  var data = '';
-  req.on('data', function(c) { data += c });
-  req.on('end', function() {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files) {
 
-    // Parse the data as x-www-form-urlencoded
-    data = querystring.parse(data);
+    // Handle errors with basic fail
+    if (err) return doResp(res, 500, 'Server Error');
 
-    // Check if this is from craiglist
-    var fromCraig = false;
-    try {
-      fromCraig = !!data.sender.match(/noreply@craigslist.org/);
-    } catch (err) {
-      fromCraig = true; // Meh
-    }
+    // Verify the request
+    if (!email.verify(fields.timestamp, fields.token, fields.signature))
+      return doResp(res, 400, 'Bad Request');
 
-    // Verify any requests from non-craiglist
-    if (!fromCraig && !email.verify(data.timestamp, data.token, data.signature)) {
-      res.writeHead(400, {'Content-Type': 'text/html; charset=utf-8'});
-      res.end('Bad Request');
-      return;
-    }
+    // check to see if this is from craigslist
+    var fromCraig = !!fields.sender.match(/noreply@craigslist.org/);
 
     // Forward the email contents to sold@hipsell.com
     email.send('sold@hipsell.com',
-               'Autoreply For: ' + data.subject,
-               '<h4>Original Sender: ' + data.sender + '</h4>' + data['body-plain'] || data);
+               'Autoreply For: ' + fields.subject,
+               '<h4>Original Sender: ' + fields.sender + '</h4><p>' + fields['body-plain'] + '</p>');
 
-    // If it's from craigslist bail out now
-    if (fromCraig) {
-      res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-      res.end('OK');
-      return;
-    }
+    // If it's from craig, finish early
+    if (fromCraig) return doResp(res, 200, 'OK');
 
-    // Fetch the relevant listing
+    // Otherwise, fetch the relevant listing
     var listing = new models.Listing();
     listing._id = id;
     db.get(listing, function(err) {
 
       // If there's no such listing, bail out
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
-        res.end('Not Found');
-        return;
-      }
+      if (err) return doResp(res, 404, 'Not Found');
 
       // Report success to mailgun
-      res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-      res.end('OK');
+      doResp(res, 200, 'OK');
 
-      // Send the autoreply to the sender -- skip if it's from craiglist
-      // though.
-      email.send(data.sender,
-                 'Re: ' + data.subject,
+      // Send the autoreply to the sender
+      email.send(fields.sender,
+                 'Re: ' + fields.subject,
                  templating['email/autoresponse'].render({listing: listing}),
-                 data.recipient);
+                 fields.recipient);
+
     });
   });
 };
